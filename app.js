d3ac7708e63b7d97ca67f9c9bcc5e7f94be21386
aalskdjfhg123 <jiangryan8@gmail.com>
2023-10-03 19:40:44 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+
+const weight = Number(prompt("Enter your weight in lbs"));
+const height = Number(prompt("Enter your height in inches"));
+
+
+
+function calc() {
+    const bmi = (weight / (height * height) * 703).toFixed(2);
+    alert(`Your BMI is ${bmi}`)
+    return bmi;
+}
+
+
+const health = function () {
+    const bmi = calc();
+
+    if (bmi < 18.5) {
+        alert("You are underweight");
+    }
+    else if (bmi > 18.5 & bmi < 25) {
+        alert("You are healthy");
+    } else {
+        alert("You are overweight");
+    }
+}
+health();
+
+
+
